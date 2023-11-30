78b1480436a530b47a1f67b7f4daad7000b7a9dd
Bins777 <147042394+Bins777@users.noreply.github.com>
2023-11-30 15:04:35 +0700

    Updated UI.cpp
    fixed the menu options and exit message, as well as added in some UI functions for displaying showtimes.

--- UI.cpp
+++ UI.cpp
@@ -8,6 +8,7 @@
 #include "UI.h"
 #include "Movie.h"
 
+
 using namespace std;
 
 void UI::welcome() {
@@ -76,32 +77,32 @@ void UI::display_current_time(time_t current_time) {
 int UI::display_start_menu() {
     int choice;
     cout << "Welcome to Nonton Kuy!" << endl;
-    cout << "1. Login" << endl;
-    cout << "2. Sign up" << endl;
-    cout << "3. Exit Nonton Kuy" << endl;
+    cout << "1. Enter to Login as admin" << endl;
+    cout << "2. Exit Nonton Kuy" << endl;
     cout << "Select an option: ";
     cin >> choice;
     return choice;
 }
 
 void UI::exit_message() {
+    refresh();
     cout << "+------------------------------------------------------------------------------------------------------------------+" << endl;
-cout << "|                        ________  _______   _______            ___    ___ ________  ___  ___                      |" << endl;            
-cout << "|                       |\\   ____\\|\\  ___ \\ |\\  ___ \\          |\\  \\  /  /|\\   __  \\|\\  \\|\\  \\                     |" << endl;       
-cout << "|                       \\ \\  \\___|\\ \\   __/|\\ \\   __/|         \\ \\  \\/  / | \\  \\|\\  \\ \\  \\\\\\  \\                    |" << endl;
-cout << "|                        \\ \\_____  \\ \\  \\_|/_\\ \\  \\_|/__        \\ \\    / / \\ \\  \\ \\  \\ \\  \\\\\\  \\                   |" << endl;
-cout << "|                         \\|____|\\  \\ \\  \\_|\\ \\ \\  \\_|\\ \\        \\/  /  /   \\ \\  \\ \\  \\ \\  \\\\\\  \\                  |" << endl;
-cout << "|                           ____\\_\\  \\ \\_______\\ \\_______\\     __/  / /      \\ \\_______\\ \\_______\\                 |" << endl;
-cout << "|                          |\\_________\\|_______|\\|_______|    |\\___/ /        \\|_______|\\|_______|                 |" << endl;
-cout << "|                           \\|_________|                      \\|___|/                                              |" << endl;
-cout <<"+------------------------------------------------------------------------------------------------------------------+" << endl << endl;
-
+    cout << "|                        ________  _______   _______            ___    ___ ________  ___  ___                      |" << endl;            
+    cout << "|                       |\\   ____\\|\\  ___ \\ |\\  ___ \\          |\\  \\  /  /|\\   __  \\|\\  \\|\\  \\                     |" << endl;       
+    cout << "|                       \\ \\  \\___|\\ \\   __/|\\ \\   __/|         \\ \\  \\/  / | \\  \\|\\  \\ \\  \\\\\\  \\                    |" << endl;
+    cout << "|                        \\ \\_____  \\ \\  \\_|/_\\ \\  \\_|/__        \\ \\    / / \\ \\  \\ \\  \\ \\  \\\\\\  \\                   |" << endl;
+    cout << "|                         \\|____|\\  \\ \\  \\_|\\ \\ \\  \\_|\\ \\        \\/  /  /   \\ \\  \\ \\  \\ \\  \\\\\\  \\                  |" << endl;
+    cout << "|                           ____\\_\\  \\ \\_______\\ \\_______\\     __/  / /      \\ \\_______\\ \\_______\\                 |" << endl;
+    cout << "|                          |\\_________\\|_______|\\|_______|    |\\___/ /        \\|_______|\\|_______|                 |" << endl;
+    cout << "|                           \\|_________|                      \\|___|/                                              |" << endl;
+    cout <<"+------------------------------------------------------------------------------------------------------------------+" << endl << endl;
+    exit(0);
 }
 
 void UI::admin_header() {
     cout << "+---------------------------------+" << endl;
     cout << "|    You're logged in as ADMIN    |" << endl;
-    cout << "+---------------------------------+" << endl << endl;
+    cout << "+---------------------------------+" << endl;
     display_current_time(time(0));
 }
 
@@ -109,9 +110,9 @@ void UI::view_admin_options() {
     cout << "Available options:" << endl;
     cout << "1. Show all movies" << endl;
     cout << "2. Show all theatres" << endl;
-    cout << "3. Add a new movie" << endl;
-    cout << "4. Delete a movie" << endl;
-    cout << "5. Show sales summary" << endl;
+    cout << "3. Order movie ticket" << endl;
+    cout << "4. Add a new movie" << endl;
+    cout << "5. Delete a movie" << endl;
     cout << "6. Logout" << endl << endl;
 
     cout << "Select an option: ";
@@ -119,6 +120,7 @@ void UI::view_admin_options() {
 }
 
 void UI::display_all_movies(vector<Movie> movies) {
+    cout << "List of available movies." << endl;
     for (int i = 0; i < movies.size(); i++) {
         cout << i + 1 << ". " << movies[i].name << endl;
     }
@@ -126,22 +128,40 @@ void UI::display_all_movies(vector<Movie> movies) {
 }
 
 void UI::display_all_theatres(vector<Theatres> theatres) {
+    cout << "List of available theatres." << endl;
     for (int i = 0; i < theatres.size(); i++) {
         cout << i + 1 << ". " << theatres[i].name << endl;
     }
     cout << endl;
 }
 
-// float showtime;
-// int showtime_hour, showtime_minute;
-// int count = 1;
-// for (int i = 0; i < movies.size(); i++) {
-//     for (int j = 0; j < movies[i].showtimes.size(); j++) {
-//         showtime = movies[i].showtimes[j];
-//         showtime_hour = showtime; // typecast into int so it truncuates the fractional part
-//         showtime_minute = (showtime - showtime_hour) * 60; // get only the fractional part and multiply with 60 minutes
-//         cout << movies[i].name << " " << showtime_hour << ":";
-//         cout << setw(2) << setfill('0') << showtime_minute; // add leading zeros up to 2 digits for the minute display
-//         cout << " | The showtime availablity: " << IsShowtimeAvailable(now -> tm_hour, now -> tm_min, showtime_hour, showtime_minute) << endl;       
-//     }
-// }
+void UI::display_showtimes(vector<Movie> movies, int movie_index, tm* now) {
+    float showtime;
+    int showtime_hour, showtime_minute;
+    cout << "The showtimes for " << movies[movie_index].name << " are:" << endl;
+    for (int i = 0; i < movies[movie_index].showtimes.size(); i++) {
+        showtime = movies[movie_index].showtimes[i];
+        showtime_hour = showtime; // typecast into int so it truncuates the fractional part
+        showtime_minute = (showtime - showtime_hour) * 60; // get only the fractional part and multiply with 60 minutes
+        cout << i + 1 << ". " << showtime_hour << ":";
+        cout << setw(2) << setfill('0') << showtime_minute; // add leading zeros up to 2 digits for the minute display
+
+        string availability = is_showtime_available(now -> tm_hour, now -> tm_min, showtime_hour, showtime_minute) ? "Available" : "Unavailable";
+        cout << " | " << availability << endl;
+    }
+    
+}
+
+void UI::convert_to_showtime(float showtime) {
+    int showtime_hour, showtime_minute;
+    showtime_hour = showtime; // typecast into int so it truncuates the fractional part
+    showtime_minute = (showtime - showtime_hour) * 60; // get only the fractional part and multiply with 60 minutes
+    cout << showtime_hour << ":";
+    cout << setw(2) << setfill('0') << showtime_minute; // add leading zeros up to 2 digits for the minute display   
+}
+
+bool UI::is_showtime_available(int current_hour, int current_minute, int showtime_hour, int showtime_minute) {
+    // returns 1 if showtime has not exceeded the current time, 0 if not.
+    if (current_hour != showtime_hour) { return current_hour < showtime_hour; }
+    else { return current_minute < showtime_minute; }
+}
